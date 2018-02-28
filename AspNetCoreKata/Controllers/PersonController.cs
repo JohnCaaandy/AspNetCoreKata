e2301f2c4bf87eeda80e47d23d05d10aebd0de42
Jack Mentzer <jackdanielmentzer@gmail.com>
2018-02-28 10:44:11 -0500

    Completed steps 1-4.. added a PersonController, a model, and a view

---
+++ AspNetCoreKata/Controllers/PersonController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreKata.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreKata.Controllers
+{
+    public class PersonController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Person>
+            {
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+                new Person {Name = "Jack Mentzer", Age = 27},
+
+            };
+            return View(list);
+        }
+    }
+}
---
+++ AspNetCoreKata/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreKata.Models
+{
+    public class Person
+    {
+        public static string Name {get; set;}
+        public static int Age { get; set; }
+        public bool isMinor => Age >= 18;
+    }
+}
