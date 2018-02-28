becff4140f6d1b91b93af2d0de4134e653ea8d8d
Jack Mentzer <jackdanielmentzer@gmail.com>
2018-02-28 11:47:06 -0500

    connected databases

---
+++ AspCoreNetKata.Shared/IProductRepository.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AspCoreNetKata.Shared
+{
+    public class Class1
+    {
+    }
+}
---
+++ AspNetCoreKata.ProjectRepository/ProductRepo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using AspCoreNetKata.Shared;
+using Dapper;
+
+namespace AspNetCoreKata.ProjectRepository
+{
+    public class ProductRepo : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepo(IDbConnection conn)
+        {
+            _connection = conn;
+        }
+        public IEnumerable<Product> GetAllProducts()
+        {
+            using (var conn = _connection)
+            {
+                conn.Open();
+                return conn.Query<Product>("SELECT * FROM Product");
+            }
+                throw new NotImplementedException();
+        }
+    }
+}
